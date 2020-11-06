46b55e551a7ae8bec6101e670cb51a141a2afdf0
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-11-06 10:32:03 +0800

    Updates

---
+++ precache-manifest.262a4ba870b8826d5f1942c38a552187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c29972a8be135bd9f1b099eb364e2e",
+    "url": "/daguan_order/index.html"
+  },
+  {
+    "revision": "4e79d963008624fecef9",
+    "url": "/daguan_order/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a77c2c5e3c26d807fad",
+    "url": "/daguan_order/static/js/2.4793c789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daguan_order/static/js/2.4793c789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e79d963008624fecef9",
+    "url": "/daguan_order/static/js/main.3ff46f86.chunk.js"
+  },
+  {
+    "revision": "97484c28cbfda51e3c77",
+    "url": "/daguan_order/static/js/runtime-main.16c62ea5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daguan_order/static/media/logo.5d5d9eef.svg"
+  }
+]);
